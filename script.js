0190a5fbe090c6ee4c9b553c83064b0d4e0f3b69
Bryan01santos <bryan381santos@gmail.com>
2023-09-07 13:24:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+    function SIM(){
+        alert("Eu te amo minha patinha  ❤️❤️❤️ ")
+        alert("Você está oficialmente convidada para o meu casamento, vá vestida de branco, e convide sua família! 👰🏻‍♀️💗💕💗💕")
+    }
+
+    function desvia(t){
+        var btn = t;
+        btn.style.position = 'absolute';
+        btn.style.top = geraPosicao (10, 90);
+        btn.style.right = geraPosicao (10, 90);
+        console.log("opa, desviei...");
+    }
+
+    function geraPosicao(min, max) {
+        return (Math.random() * (max - min) + min) + '%';
+    }
